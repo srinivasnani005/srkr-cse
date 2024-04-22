e74a252990daeaa14c3a2997746a50f92a69ec17
Pragada N D S Sai Srinivas <121677119+srinivasnani005@users.noreply.github.com>
2024-04-22 10:13:06 +0530

    Update verify.php

--- Student/verify.php
+++ Student/verify.php
@@ -50,7 +50,7 @@ if(isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="css/t1.css">
+    <link rel="stylesheet" href="../css/t1.css">
     <style>
           body {
                                 margin: 0;
